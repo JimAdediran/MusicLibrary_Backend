22e30149181fa8623f77d63b84807ddc1a8a51ea
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-09-11 13:08:15 -0400

    views functions created to get song by ID and to delete a song

--- songs/urls.py
+++ songs/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.SongList.as_view()),
-   # path('<int:pk>/', views.SongDetail.as_view()),
+    path('<int:pk>/', views.SongDetail.as_view()),
 ]
