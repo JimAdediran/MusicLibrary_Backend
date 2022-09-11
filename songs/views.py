0dc4251616f512bc49187b9e88512808b75cc65b
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-09-11 12:49:56 -0400

    created views for get and post songs + added 3 songs to database

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import Http404
+
+import songs
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from songs import serializers
 
 # Create your views here.
+class SongList(APIView):
+
+    def get(self, request):
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
